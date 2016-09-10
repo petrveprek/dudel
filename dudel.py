4437fb5ae992622cb5dca31f4e5e30d3c0e2aa5d
Petr Vepřek <petr.veprek.cz420@gmail.com>
2016-09-10 18:13:34 +0100

    Scan rate

--- dudel.py
+++ dudel.py
@@ -2,11 +2,13 @@
 # Copyright (c) 2016 Petr Veprek
 """Duplicate Delete"""
 
-import argparse, os, string, sys, time
+import argparse, enum, os, string, sys, time
 
 TITLE = "Duplicate Delete"
 VERSION = "0.1"
 VERBOSE = False
+class Mode(enum.Enum): plain = 0; grouped = 1; gazillion = 2
+MODE = Mode.gazillion
 MIN_WIDTH = 9+0+3 # intro + directory + ellipsis
 MAX_WIDTH = os.get_terminal_size().columns if sys.stdout.isatty() else 80
 WIDTH = MAX_WIDTH
@@ -21,6 +23,25 @@ def printable(str, max):
     str = "".join([char if char in string.printable else "_" for char in str])
     if len(str) > max: str = str[:max-3] + "..."
     return str
+
+def plain(num):
+    return "{}".format(num)
+
+def grouped(num):
+    return "{:,}".format(num)
+
+def gazillion(num, suffix="B"):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if num < 1024.0:
+            return "{:.{}f}{}{}".format(num, 1 if num % 1 > 0 else 0, unit, suffix)
+        num /= 1024.0
+    return "{:.{}f}{}{}".format(num, 1 if num % 1 > 0 else 0, 'Yi', suffix)
+
+def format(num, mode=Mode.plain):
+    return(
+        grouped(num)   if mode == Mode.grouped   else
+        gazillion(num) if mode == Mode.gazillion else
+        plain(num))
 
 def main():
     print("{} {}".format(TITLE, VERSION))
@@ -44,11 +65,26 @@ def main():
     if not silent:
         print("Analyzing {}".format(directory))
         BACKTRACK = ("\r" if width < MAX_WIDTH else "\033[F") if sys.stdout.isatty() else "\n"
+    started = time.time()
+    numDirs, numFiles = (0,) * 2
     for path, dirs, files in os.walk(directory):
         if not silent:
             print("Scanning {: <{}}".format(printable(path, width-9), width-9), end=BACKTRACK)
+        dirs  = list(filter(os.path.isdir,  map(lambda dir:  os.path.join(path, dir),  dirs)))
+        files = list(filter(os.path.isfile, map(lambda file: os.path.join(path, file), files)))
+        numDirs  += len(dirs)
+        numFiles += len(files)
     if not silent:
         print("         {: <{}}".format("", width-9), end=BACKTRACK)
+        seconds = max(1, round(time.time() - started))
+        dirRate  = round(numDirs  / seconds, 1)
+        fileRate = round(numFiles / seconds, 1)
+        print("Found {} director{} with {} file{} in {} second{} ({} director{}/s, {} file{}/s)".format(
+            format(numDirs,  mode=Mode.grouped), "y" if numDirs  == 1 else "ies",
+            format(numFiles, mode=Mode.grouped), ""  if numFiles == 1 else "s",
+            format(seconds,  mode=Mode.grouped), ""  if seconds  == 1 else "s",
+            format(dirRate,  mode=Mode.grouped), "y" if dirRate  == 1 else "ies",
+            format(fileRate, mode=Mode.grouped), ""  if fileRate == 1 else "s"))
     
     if VERBOSE:
         elapsed = time.time() - start
